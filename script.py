12b577e7462b97d8f2565215f541f07a1de8ac63
William Berard <berardwilliam@gmail.com>
2019-01-15 11:08:27 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', titre="LeTitre", mots="test")
+    return render_template('index.html', titre="LeTitre", mots="[test1,test2]")
 
 if __name__ == '__main__':
     app.run(debug=True)
