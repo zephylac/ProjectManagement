21587a69d1ba123ab69fdc66c33e1c3155e53207
Thibault Brocherieux <tbrocherieux@ippon.fr>
2019-01-15 15:02:58 +0100

    fix

--- app.py
+++ app.py
@@ -21,21 +21,21 @@ def literraire():
     return render_template('francais.html')
 
 @app.route('/exo/math/')
-def index2():
+def math():
     return render_template('math.html')
 
 @app.route('/exo/math/easy')
-def index2():
+def math_easy():
     var1, var2, var3 = math_custom.easy()
     return render_template('easy.html',var1=var1,var2=var2,var3=var3)
 
 @app.route('/exo/math/medium')
-def index2():
+def math_medium():
     var1, var2, var3 = math_custom.medium()
     return render_template('medium.html',var1=var1,var2=var2,var3=var3)
 
 @app.route('/exo/math/hard')
-def index2():
+def math_hard():
     var1, var2, var3 = math_custom.hard()
     return render_template('hard.html',var1=var1,var2=var2,var3=var3)
 
