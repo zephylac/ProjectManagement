8ec6d2526fad715e386b4b2ee3f7de5b8dfb8d65
zephylac <zephylac@users.noreply.github.com>
2019-01-15 11:41:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from redis import Redis
+
 app = Flask(__name__)
+redis = Redis(host='flask-redis', port=6379)
+
+@app.route('/hello')
+def hello():
+    redis.incr('hits')
+    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
 
 @app.route('/')
 def index():
     return render_template('index.html', titre="LeTitre", mots="[test1,test2,test3]")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
