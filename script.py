50220e901922bcbc70d0da346cd5001160911c64
William Berard <berardwilliam@gmail.com>
2019-01-15 11:06:40 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html', titre="LeTitre", mots="test")
+
+if __name__ == '__main__':
+    app.run(debug=True)
